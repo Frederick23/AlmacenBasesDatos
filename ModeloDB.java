3942fbb48e6163628becd377c9af8c3c63f2067d
Frederick23 <35486596+Frederick23@users.noreply.github.com>
2018-05-19 16:21:03 +0200

    Add files via upload

--- ModeloDB.java
+++ ModeloDB.java
@@ -14,13 +14,14 @@ import java.sql.*;
 public class ModeloDB extends ModeloAbs
 {
     // instance variables - replace the example below with your own
-   static final String DATABASE USTATIC FINAL String DATABASE_URL = "jdbc:mysql://localhost/productosdb";
-   static final String USER = "alumno";
+   static final String DATABASE_URL = "jdbc:mysql://localhost/productosdb";
+   static final String USER = "Diego";
    static final String PASSWORD = "alumno";
 
     public Connection con = null;
     public Statement st = null;
-    public ResulSet rs = null;  
+    public ResultSet rs = null; 
+     
     /**
      * Constructor for objects of class ModeloDB
      * Establece la conexion a la base de datos
@@ -28,12 +29,16 @@ public class ModeloDB extends ModeloAbs
     public ModeloDB()
     {
         try{
+        	Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(DATABASE_URL,USER,PASSWORD);
             st = con.createStatement();
 
         }catch (SQLException e){
             System.out.println("Error al conectarse");
-            sql.printStackTrace();
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e1){
+        	System.out.println("Error de driver");
         }
     }
 
@@ -41,11 +46,11 @@ public class ModeloDB extends ModeloAbs
     public boolean insertarProducto ( Producto p){
         try{
 
-            st.executeUpdate("INSERT INTO productos values("+p.getCodigo()+","+"p.getNombre()"+","+p.getStock()+","+p.getStock_min()+","+p.getPrecio()+")");
+            st.executeUpdate("INSERT INTO productos values("+p.getCodigo()+","+"'"+p.getNombre()+"'"+","+p.getStock()+","+p.getStock_min()+","+p.getPrecio()+")");
             return true;
 
         }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
         }
 
     return false; 
@@ -58,7 +63,7 @@ public class ModeloDB extends ModeloAbs
             st.executeUpdate("DELETE from productos where codigo = "+codigo);
             return true;
         }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
         }
     return false;    
     }
@@ -68,17 +73,19 @@ public class ModeloDB extends ModeloAbs
         Producto aux = null;
 
         try{
+        	
+            rs = st.executeQuery("select * from productos where codigo  ="+codigo);
+            if(rs.next()) {
+            	aux = new Producto(rs.getInt(1),rs.getString(2));
+                aux.setStock(rs.getInt(3));
+                aux.setStock_min(rs.getInt(4));
+                aux.setPrecio(rs.getFloat(5));
 
-            rs = st.executeQuery("select * from productos where codigo  ="+c);
-            rs.next();
-            aux = new Producto(rs.getInt(1),rs.getString(2));
-            aux.setStock(rs.getInt(3));
-            aux.setStock_min(rs.getInt(4));
-            aux.setPrecio(rs.getFloat(5));
-
+            }
+            
 
         }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
         }
 
     return aux;    
@@ -97,17 +104,17 @@ public class ModeloDB extends ModeloAbs
 
 
         }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
         }
     }
     
     //UPDATE
     boolean modificarProducto (Producto nuevo){
         try{
-            st.executeUpdate("UPDATE PRODUCTOS set stock = "+nuevo.getStock()+" set stock_min = "+nuevo.getStock_min()+ "set precio = "+nuevo.getPrecio() + " where codigo = " +nuevo.getCodigo());
+            st.executeUpdate("UPDATE PRODUCTOS set stock = "+nuevo.getStock()+" , stock_min = "+nuevo.getStock_min()+ " , precio = "+nuevo.getPrecio() + " where codigo = " +nuevo.getCodigo());
             return true;
         }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
         }
     return false;    
     }
@@ -119,15 +126,18 @@ public class ModeloDB extends ModeloAbs
        // Relleno el array list con los resultados de al consulta
        try{
             rs = st.executeQuery("SELECT * from productos");
-            Producto aux = new Producto(rs.getInt(1),rs.getString(2));
-            aux.setStock(rs.getInt(3));
-            aux.setStock_min(rs.getInt(4));
-            aux.setPrecio(rs.getFloat(5));
-            lista.insertarProducto(aux);
+            while(rs.next()) {
+            	Producto aux = new Producto(rs.getInt(1),rs.getString(2));
+                aux.setStock(rs.getInt(3));
+                aux.setStock_min(rs.getInt(4));
+                aux.setPrecio(rs.getFloat(5));
+                lista.add(aux);
+            }
+            
 
 
        }catch(SQLException e){
-            sql.printStackTrace();
+            e.printStackTrace();
        }
        
        return lista.iterator();
